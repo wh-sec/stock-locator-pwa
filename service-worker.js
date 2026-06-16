4d1f8497a30034d478ff9fdf31907c6786cbd352
wh-sec <noelsantosjr6@gmail.com>
2026-06-16 09:56:40 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'stock-locator-v25'; // Changing this version forces a cache reset
+const CACHE_NAME = 'stock-locator-v26'; // Changing this version forces a cache reset
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
