5c8bc1f097ba8cbeffe8514a1d4800a99a6c2be6
wh-sec <noelsantosjr6@gmail.com>
2026-07-14 08:07:18 +0800

    Update service worker cache version to v81

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'stock-locator-v80'; // Changing this version forces a cache reset
+const CACHE_NAME = 'stock-locator-v81'; // Changing this version forces a cache reset
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
